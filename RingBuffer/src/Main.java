27040fef62bea25c1b5f8b0d951ec75ca91359b2
ryannemiroff <ryannemiroff@10.0.1.107>
2015-04-18 16:21:49 -0700

    Removed an unnecessary thing.

--- RingBuffer/src/Main.java
+++ RingBuffer/src/Main.java
@@ -2,12 +2,7 @@ import java.util.Random;
 
 public class Main {
 	public static void main(String[] args) throws InterruptedException {
-		int capcity = 9;
 		RingBuffer rb = new RingBuffer(9);
-		Random random = new Random();
-		for (int i = 0; i < capcity / 2; i++) {
-			rb.addNumber(random.nextInt(100) + 1);
-		}
 		Consumer c = new Consumer(rb);
 		Producer p = new Producer(rb);
 		Thread.sleep(10000);
